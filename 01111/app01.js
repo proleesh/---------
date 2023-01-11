badbac0cff4f0893078ed1800edc7a9f49cc09f4
Sung-Hyuk Lee <merci726@yahoo.com>
2023-01-11 15:18:34 +0900

    second commit

--- 01111/app01.js
+++ 01111/app01.js
@@ -1,1 +1,40 @@
 console.log("안녕하세요.");
+var Person = /** @class */ (function () {
+    function Person(name, age, country) {
+        this.name = name;
+        this.age = age;
+        this.country = country;
+    }
+    Person.prototype.greeting = function () {
+        console.log("Hello ".concat(this.name, ", You're ").concat(this.age, " years old."));
+    };
+    Person.prototype.from = function () {
+        console.log(this.country);
+    };
+    return Person;
+}());
+var sunghyuk = new Person("Sung-Hyuk", 24, "South Korea");
+sunghyuk.greeting();
+sunghyuk.from();
+var Human = {
+    name: "Sung-Hyuk",
+    age: 24,
+    pet: {
+        name: "카카오",
+        age: 19
+    },
+    sayHello: function () {
+        console.log("환영합니다.");
+    }
+};
+Human.sayHello();
+if (typeof Human === "object") {
+    console.log("객체 타입입니다.");
+}
+else if (typeof Human === "string") {
+    console.log("원시 타입입니다.");
+}
+var fruits = ['apple', 'pineapple', 'strawberry'];
+fruits.forEach(function (a) {
+    console.log(a);
+});
--- 01111/app01.ts
+++ 01111/app01.ts
@@ -1,1 +1,41 @@
-console.log("안녕하세요.")
+console.log("안녕하세요.");
+class Person {
+  name: string;
+  age: number;
+  country: string;
+  constructor(name: string, age: number, country: string) {
+    this.name = name;
+    this.age = age;
+    this.country = country;
+  }
+  greeting() {
+    console.log(`Hello ${this.name}, You're ${this.age} years old.`);
+  }
+  from() {
+    console.log(this.country);
+  }
+}
+let sunghyuk = new Person("Sung-Hyuk", 24, "South Korea");
+sunghyuk.greeting();
+sunghyuk.from();
+const Human = {
+  name: "Sung-Hyuk",
+  age: 24,
+  pet: {
+    name: "카카오",
+    age: 19,
+  },
+  sayHello: function () {
+    console.log("환영합니다.");
+  },
+};
+Human.sayHello();
+if (typeof Human === "object") {
+  console.log("객체 타입입니다.");
+} else if (typeof Human === "string") {
+  console.log("원시 타입입니다.");
+}
+const fruits = ['apple', 'pineapple', 'strawberry']
+fruits.forEach((a:any)=>{
+    console.log(a)
+})
